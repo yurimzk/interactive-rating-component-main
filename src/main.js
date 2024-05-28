5a58d393b42c79949087099a5a99867c492fc287
Yuri Tagomori <goncalvessyuri@gmail.com>
2024-05-28 10:01:46 -0300

    add rating click behavior

---
+++ src/main.js
@@ -0,0 +1,43 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const rateOne = document.querySelector('.rate-one');
+  const rateTwo = document.querySelector('.rate-two');
+  const rateThree = document.querySelector('.rate-three');
+  const rateFour = document.querySelector('.rate-four');
+  const rateFive = document.querySelector('.rate-five');
+
+  function toggleRateOne() {
+    rateOne.addEventListener('click', () => {
+      rateOne.classList.toggle('active-rate');
+    });
+  }
+
+  function toggleRateTwo() {
+    rateTwo.addEventListener('click', () => {
+      rateTwo.classList.toggle('active-rate');
+    });
+  }
+
+  function toggleRateThree() {
+    rateThree.addEventListener('click', () => {
+      rateThree.classList.toggle('active-rate');
+    });
+  }
+
+  function toggleRateFour() {
+    rateFour.addEventListener('click', () => {
+      rateFour.classList.toggle('active-rate');
+    });
+  }
+
+  function toggleRateFive() {
+    rateFive.addEventListener('click', () => {
+      rateFive.classList.toggle('active-rate');
+    });
+  }
+
+  toggleRateOne();
+  toggleRateTwo();
+  toggleRateThree();
+  toggleRateFour();
+  toggleRateFive();
+});
